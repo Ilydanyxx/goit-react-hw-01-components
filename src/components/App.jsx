94dbff11b485a32ccf8c6e6c5af83aa4bf09c21f
Ilydanyxx <danya.chuyko@icloud.com>
2023-08-04 00:07:49 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,11 @@ import Profile from './Profile/Profile';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
-import transactions from '../transactions.json';
+
 import { Container } from '@mui/material';
 import { Component } from 'react';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export default class App extends Component {
   render() {
