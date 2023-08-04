44f87d8603fef7627cb4d2c1b2119e5d3925b0ed
Ilydanyxx <danya.chuyko@icloud.com>
2023-08-04 18:10:30 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from './TransactionHistory.module.css';
+
 export default function TransactionHistory({ items }) {
   return (
     <table>
       <thead>
         <tr>
-          <th className="row">Type</th>
-          <th className="row">Amount</th>
-          <th className="row">Currency</th>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(({ id, type, amount, currency }) => {
           return (
-            <tr className="row" id={id} key={id}>
+            <tr id={id} key={id}>
               <td>{type}</td>
               <td>{amount}</td>
               <td>{currency}</td>
